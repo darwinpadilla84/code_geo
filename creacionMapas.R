be1c82ca3ce9d774d81bc1413cf1e5d8b0713ee8
AndreaValeria16 <133240232+AndreaValeria16@users.noreply.github.com>
2023-05-11 09:43:57 -0500

    Update creacionMapas.R
    Instalación de paquetes

--- creacionMapas.R
+++ creacionMapas.R
@@ -1,1 +1,12 @@
+# Packages for reading data and data carpentry
+library(readr)
+library(tibble)
+library(janitor)
+library(dplyr)
+# Packages for handling spatial data
+library(sf)
+# Packages for visualisation and mapping
+library(ggplot2)
+library(ggspatial)
+
 
